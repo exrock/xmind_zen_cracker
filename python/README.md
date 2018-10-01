5aa2fb90bf7c4873f7340e4a1cc4136f8514de87
Xmader <xmader@outlook.com>
2018-10-01 15:41:39 +0800

    适配MacOS

--- python/README.md
+++ python/README.md
@@ -2,11 +2,28 @@
 
 ## 运行方法
 
+### Windows 和 Linux
+
 1. 下载[最新版本可执行文件](https://github.com/Xmader/xmind_zen_cracker/releases/latest)或自行构建
 
-2. 将 `xmind_zen_cracker.exe`(Windows)或`xmind_zen_cracker`(Linux) 复制到`Xmind Zen`的安装目录 (含有`locales`、`resources`等文件夹)
+2. 将 `xmind_zen_cracker_Windows.exe`(Windows)或`xmind_zen_cracker_Linux`(Linux) 复制到`Xmind Zen`的安装目录 (含有`locales`、`resources`等文件夹)
 
-3. 运行 `xmind_zen_cracker.exe`(Windows)或`xmind_zen_cracker`(Linux)
+3. 运行 `xmind_zen_cracker_Windows.exe`(Windows)或`xmind_zen_cracker_Linux`(Linux)
+
+### MacOS
+
+1. 下载[最新版本可执行文件](https://github.com/Xmader/xmind_zen_cracker/releases/latest)或自行构建
+
+2. 将 `xmind_zen_cracker_MacOS` 复制到 `Xmind Zen.app` 所在目录
+
+3. 运行 `xmind_zen_cracker_MacOS`
+
+> 可能需要先运行 `chmod +x xmind_zen_cracker_MacOS` 添加可执行权限
+
+
+## 备注
+
+只支持64位操作系统
 
 ## 自行构建
 
--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Xmind Zen 破解器
 import os
+import platform
 import sys
 
 import uac
@@ -13,18 +14,23 @@ add_css = """
 }
 """
 
+if platform.system().lower() == "darwin":
+    app_out_dir = "./XMind ZEN.app/Contents/Resources/app/out/"
+else:
+    app_out_dir = "./resources/app/out/"
+
 blank_files = [
-    "./resources/app/out/modal-activateAlert.js",
-    "./resources/app/out/imgs/pdf-footer-zh-CN.svg",
-    "./resources/app/out/imgs/png-watermark-zh-CN.svg",
-    "./resources/app/out/imgs/print-watermark-zh-CN.svg"
+    "modal-activateAlert.js",
+    "imgs/pdf-footer-zh-CN.svg",
+    "imgs/png-watermark-zh-CN.svg",
+    "imgs/print-watermark-zh-CN.svg"
 ]
 
 for i in blank_files:
-    open(i, "wb").close()
+    open(app_out_dir + i, "wb").close()
 
-with open("./resources/app/out/new.css", "ab") as f:
+with open(app_out_dir + "new.css", "ab") as f:
     f.write(add_css)
 
-with open("./resources/app/out/renderer.css", "ab") as f:
+with open(app_out_dir + "renderer.css", "ab") as f:
     f.write(add_css)
